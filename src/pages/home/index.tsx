051d3d482354496f9b560bfe495c647fe9b145e6
Ronald Oliveira <164264112+ronald1134@users.noreply.github.com>
2025-07-11 14:08:19 -0300

    aplicando validação no inputs da página home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,7 @@
 import { Play } from "phosphor-react";
-import { useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form';
+import {zodResolver} from '@hookform/resolvers/zod';
+import * as zod from 'zod';
 
 import {
     CountdownContainer,
@@ -11,9 +13,23 @@ import {
     TaskInput
 } from "./style";
 
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutesAmount: zod.number()
+        .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
+        .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+});
+
+
 export function Home() {
     // Configuração do react-hook-form
-    const { register, handleSubmit, watch } = useForm()
+    const { register, handleSubmit, watch } = useForm({
+        resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues: {
+            task: '',
+            minutesAmount: 0,
+        },
+    })
 
     function handleCreateNewCycle(data: unknown) {//IREI ARRUMAR AQUI DPS
         console.log(data);
@@ -65,7 +81,7 @@ export function Home() {
                     <span>0</span>
                 </CountdownContainer>
 
-                <StartCountdownButton disabled={!isSubmitDisabled} type="submit">
+                <StartCountdownButton disabled={isSubmitDisabled} type="submit">
                     <Play size={24} />
                     Começar
                 </StartCountdownButton>
